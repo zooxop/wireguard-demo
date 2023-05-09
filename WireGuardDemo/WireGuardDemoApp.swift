c5d44517c7bdf8cb8fc25cc8d48b910c450e8166
cheolhyun mun <cheolhyun.mun@monitorapp.com>
2023-05-09 09:43:07 +0900

    add: initial project

---
+++ WireGuardDemo/WireGuardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WireGuardDemoApp.swift
+//  WireGuardDemo
+//
+//  Created by 문철현 on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct WireGuardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
