118f29f9a5434367823c3e221facc8cbe9ec97e3
CheolHyun Mun <zooxop@gmail.com>
2023-06-30 15:18:20 +0900

    feat: iOS Platform 추가

---
+++ WireGuardDemoiOS/WireGuardDemoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WireGuardDemoiOSApp.swift
+//  WireGuardDemoiOS
+//
+//  Created by 문철현 on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct WireGuardDemoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
